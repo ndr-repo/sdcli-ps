c95f5ed53520dd69f559163e95928540309c9282
weekndr_sec <172009322+ndr-repo@users.noreply.github.com>
2025-04-11 17:05:16 -0400

    Update sdcli.ps1

--- sdcli/sdcli.ps1
+++ sdcli/sdcli.ps1
@@ -1,5 +1,5 @@
 $Key = Get-Content .\key.txt
 $QueryLink = 'https://api.subbdom.com/v1/search?z='+$args[0]
 $ResultFile = $args[1]
-(Invoke-WebRequest  -Headers @{'x-api-key'="$Key"} -UserAgent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' -Uri $QueryLink -Method Get).Content | ConvertFrom-Json | Out-String -Stream | Sort-Object -Unique | Out-File "$ResultFile".txt utf8 -Force
-Write-Host "Results saved to $ResultFile.txt" -ForegroundColor Red
+(Invoke-WebRequest  -Headers @{'x-api-key'="$Key"} -UserAgent 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' -Uri $QueryLink -Method Get).Content | ConvertFrom-Json | Out-String -Stream | Sort-Object -Unique | Out-File "$ResultFile" utf8 -Force
+Write-Host "Results saved to $ResultFile" -ForegroundColor Red
